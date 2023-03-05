76031be24498547b4a2e16c4da3cc9460ff5371d
ItzCrazyKns <kushagra20103@gmail.com>
2023-03-05 11:14:37 +0530

    Add Servers View In Admin Area 🥀

--- backend/admin/users.js
+++ backend/admin/users.js
@@ -75,13 +75,3 @@ console.error(error);
 });
   })
 };
-
-function makeid(length) {
-  let result = '';
-  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_!<?/';
-  let charactersLength = characters.length;
-  for (let i = 0; i < length; i++) {
-     result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
-}
